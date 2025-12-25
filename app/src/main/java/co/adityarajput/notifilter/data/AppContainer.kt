37cce2ba7fd42d3139301a97ce99ee94c86911d7
Aditya Rajput <adiraj20072002@gmail.com>
2025-12-25 18:01:02 +0530

    Add zombie code for demo data seeding

--- app/src/main/java/co/adityarajput/notifilter/data/AppContainer.kt
+++ app/src/main/java/co/adityarajput/notifilter/data/AppContainer.kt
@@ -1,8 +1,12 @@
 package co.adityarajput.notifilter.data
 
 import android.content.Context
+import co.adityarajput.notifilter.data.filter.Action
+import co.adityarajput.notifilter.data.filter.Filter
 import co.adityarajput.notifilter.data.filter.FiltersRepository
+import co.adityarajput.notifilter.data.notification.Notification
 import co.adityarajput.notifilter.data.notification.NotificationsRepository
+import kotlinx.coroutines.runBlocking
 
 class AppContainer(private val context: Context) {
     val filtersRepository: FiltersRepository by lazy {
@@ -10,5 +14,52 @@ class AppContainer(private val context: Context) {
     }
     val notificationsRepository: NotificationsRepository by lazy {
         NotificationsRepository(NotiFilterDatabase.getDatabase(context).notificationDao())
+    }
+
+    fun seedDemoData() {
+        runBlocking {
+            filtersRepository.create(
+                Filter(
+                    "com.google.android.deskclock",
+                    "Upcoming alarm",
+                    Action.DISMISS,
+                    "",
+                    87,
+                ),
+            )
+            filtersRepository.create(
+                Filter(
+                    "com.wssyncmldm",
+                    "software update",
+                    Action.TAP,
+                    "Remind me",
+                    23,
+                ),
+            )
+            notificationsRepository.save(
+                Notification(
+                    "Upcoming alarm",
+                    "Wed 8:30 AM - Wake up",
+                    "com.wssyncmldm",
+                    System.currentTimeMillis() - 3456789 - 4 * 3600000,
+                ),
+            )
+            notificationsRepository.save(
+                Notification(
+                    "Upcoming alarm",
+                    "Wed 11:50 AM - Exercise",
+                    "com.wssyncmldm",
+                    System.currentTimeMillis() - 3456789,
+                ),
+            )
+            notificationsRepository.save(
+                Notification(
+                    "Download paused",
+                    "A software update is available.",
+                    "com.wssyncmldm",
+                    System.currentTimeMillis() - 1234567,
+                ),
+            )
+        }
     }
 }
