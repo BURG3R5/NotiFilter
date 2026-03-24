adc7edc49adc607bd7e98b4cd41470f9d81759ba
Aditya Rajput <adiraj20072002@gmail.com>
2026-03-24 14:12:47 +0530

    Delay correctly for overnight schedules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "co.adityarajput.notifilter"
         minSdk = 29
         targetSdk = 36
-        versionCode = 30
-        versionName = "4.10.0"
+        versionCode = 31
+        versionName = "4.10.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
