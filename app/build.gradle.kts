eb8a4ac49e4a80587e2e1ffb9559ef10cbeef886
Aditya Rajput <adiraj20072002@gmail.com>
2026-06-16 00:31:12 +0530

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "co.adityarajput.notifilter"
         minSdk = 29
         targetSdk = 36
-        versionCode = 32
-        versionName = "4.11.0"
+        versionCode = 33
+        versionName = "4.12.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
